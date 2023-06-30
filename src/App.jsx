1813b827f017269020c4ac9a8e5dedfb6ce3e318
razeeth11 <razeeth011@gmail.com>
2023-06-30 08:10:04 +0530

    CSS-Updated

--- src/App.jsx
+++ src/App.jsx
@@ -11,13 +11,26 @@ function App() {
     <div className="App">
       <Header />
       <BodyOne />
+      <MicComp/>
       <BodyTwo />
       <BodyThree />
       <CustomerReview />
       <Footer />
-      {/* <img className="image" src="public/images/polygon-2.png" alt="" /> */}
     </div>
   );
+}
+
+function MicComp(){
+  return(
+    <div className="div">
+      <div className="mic-comp">
+      <img src="src/images/pngwing 1.png" alt="" />
+    </div>
+    <div className="podcast">
+      <img src="public/images/group-79.png" alt=""/>
+    </div>
+    </div>
+  )
 }
 
 
